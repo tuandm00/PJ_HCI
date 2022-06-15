ec5e3dd51025eeee77826f81fd2a84d853347664
Tuandm <dominhtuan23102000@gmail.com>
2022-06-15 20:20:29 +0700

    done danh sach cong viec layout

--- app/src/main/java/com/example/pj_hci_v2/Freelancer_P5.java
+++ app/src/main/java/com/example/pj_hci_v2/Freelancer_P5.java
@@ -7,39 +7,37 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Freelancer_P4 extends AppCompatActivity {
-    Button btnLienhetructiep, btnQuaylai, btnTrangchu;
+public class Freelancer_P5 extends AppCompatActivity {
+    Button btnLienhengay, btnQuaylai, btnTrangchu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_freelancer_p4);
+        setContentView(R.layout.activity_freelancer_p5);
 
-        btnLienhetructiep = (Button) findViewById(R.id.buttonLienhetructiep);
+        btnLienhengay = (Button) findViewById(R.id.buttonLienhengay);
         btnQuaylai = (Button) findViewById(R.id.buttonQuaylai);
         btnTrangchu = (Button) findViewById(R.id.buttonTrangchu);
 
-        btnLienhetructiep.setOnClickListener(new View.OnClickListener() {
+        btnLienhengay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Freelancer_P4.this, Freelancer_P5.class);
+                Intent intent = new Intent(Freelancer_P5.this, Freelancer_P6.class);
                 startActivity(intent);
             }
         });
         btnQuaylai.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Freelancer_P4.this, Freelancer_P3.class);
+                Intent intent = new Intent(Freelancer_P5.this, Freelancer_P4.class);
                 startActivity(intent);
             }
         });
         btnTrangchu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Freelancer_P4.this, Freelancer_P1.class);
+                Intent intent = new Intent(Freelancer_P5.this, Freelancer_P1.class);
                 startActivity(intent);
             }
         });
-
-
     }
 }
--- app/src/main/java/com/example/pj_hci_v2/Freelancer_P6.java
+++ app/src/main/java/com/example/pj_hci_v2/Freelancer_P6.java
@@ -7,39 +7,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Freelancer_P4 extends AppCompatActivity {
-    Button btnLienhetructiep, btnQuaylai, btnTrangchu;
+public class Freelancer_P6 extends AppCompatActivity {
+    Button btnQuaylai, btnTrangchu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_freelancer_p4);
+        setContentView(R.layout.activity_freelancer_p6);
 
-        btnLienhetructiep = (Button) findViewById(R.id.buttonLienhetructiep);
         btnQuaylai = (Button) findViewById(R.id.buttonQuaylai);
         btnTrangchu = (Button) findViewById(R.id.buttonTrangchu);
 
-        btnLienhetructiep.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Freelancer_P4.this, Freelancer_P5.class);
-                startActivity(intent);
-            }
-        });
         btnQuaylai.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Freelancer_P4.this, Freelancer_P3.class);
+                Intent intent = new Intent(Freelancer_P6.this, Freelancer_P5.class);
                 startActivity(intent);
             }
         });
         btnTrangchu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Freelancer_P4.this, Freelancer_P1.class);
+                Intent intent = new Intent(Freelancer_P6.this, Freelancer_P1.class);
                 startActivity(intent);
             }
         });
-
-
     }
 }
